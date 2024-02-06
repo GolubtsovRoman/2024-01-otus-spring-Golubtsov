ceeec6a21ad396b80fdca2a97cce8d30ffd7f7a1
Roman Golubtsov <roman.golubtsov@taskdata.ru>
2024-02-06 22:31:10 +0300

    Добавлен тест и ci на сборку

---
+++ hw01-xml-config/src/test/java/ru/otus/hw/ApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.otus.hw;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    @DisplayName("check test")
+    void init() {
+        System.out.println("empty test");
+    }
+
+}
