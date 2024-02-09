0e7dd3258589121b6ed3c339afe95d0f4caef06c
Roman Golubtsov <77449381+GolubtsovRoman@users.noreply.github.com>
2024-02-09 17:46:00 +0300

    ДЗ#01 Приложение по проведению тестирования студентов - только вывод вопросов и вариантов ответа (если имеются)
    Цель: 
    Создать приложение с помощью Spring IoC, чтобы познакомиться с основной функциональностью IoC, на которой строится весь Spring.
    
    Результат: 
    Простое приложение, сконфигурированное XML-контекстом.

---
+++ hw01-xml-config/src/test/java/ru/otus/hw/ApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.otus.hw;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    @DisplayName("check live test")
+    void init() {
+        System.out.println("It are live!");
+    }
+
+}
