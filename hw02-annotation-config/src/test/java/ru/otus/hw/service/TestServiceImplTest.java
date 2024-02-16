fe604ce7d75d9fbb271d736444745b451fd4ecf5
Roman Golubtsov <77449381+GolubtsovRoman@users.noreply.github.com>
2024-02-16 17:52:31 +0300

    ДЗ№2 Приложение по проведению тестирования студентов (с самим тестированием) (#2)
    * ДЗ№2 Приложение по проведению тестирования студентов (с самим тестированием)
    
    * Исправлены замечания из ревью по тестам
    * Исправлены замечания из ревью по сервису тестирования студентов.

---
+++ hw02-annotation-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,55 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.help.QuestionHelper;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+@DisplayName("Process test student")
+@ExtendWith(MockitoExtension.class)
+class TestServiceImplTest {
+
+    @Mock
+    private IOService ioService;
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @InjectMocks
+    private TestServiceImpl testServiceImpl;
+
+
+    @Test
+    @DisplayName("should be execute test for a student")
+    void executeTestFor() {
+        doNothing().when(ioService).printLine(anyString());
+        doNothing().when(ioService).printFormattedLine(anyString());
+        when(ioService.readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString())).thenReturn(1);
+
+        var questions = QuestionHelper.makeExampleQuestions();
+        var countOfQuestions = questions.size();
+        when(questionDao.findAll()).thenReturn(questions);
+
+        Student student = new Student("Golubtsov", "Roman");
+        testServiceImpl.executeTestFor(student);
+
+        int countOfPrintLine = 1 + countOfQuestions * 2 + 1; // empty line + questions * (border + question) +  border
+        verify(ioService, times(countOfPrintLine)).printLine(anyString());
+        verify(ioService).printFormattedLine(anyString());
+        verify(ioService, times(countOfQuestions)).readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString());
+        verify(questionDao).findAll();
+    }
+
+}
