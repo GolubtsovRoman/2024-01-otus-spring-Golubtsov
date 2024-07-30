5a305f350acd2b330a009e3f4b43aa342b8948e7
Roman Golubtsov <roman.golubtsov@taskdata.ru>
2024-07-30 19:25:25 +0300

    Проектная работа
    - мелкие правки

--- project-work/report-sender/src/main/java/ru/otus/project/report/sender/config/rabbit/RabbitListenerConfig.java
+++ project-work/report-sender/src/main/java/ru/otus/project/report/sender/config/rabbit/RabbitListenerConfig.java
@@ -15,17 +15,17 @@ public class RabbitListenerConfig {
     private final MessageProcessService messageProcessService;
 
 
-    @RabbitListener(queues = {"#{birthdaysQueue.getName()}"})
+    @RabbitListener(queues = {"${service.mq.queues.birthdays}"})
     public void listenBirthdays(BirthdayInfo birthdayInfo) {
         messageProcessService.processOnMessage(birthdayInfo);
     }
 
-    @RabbitListener(queues = {"#{expiringAccountsQueue.getName()}"})
+    @RabbitListener(queues = {"${service.mq.queues.expiring-accounts}"})
     public void listenExpiringAccounts(ExpireAccounts expireAccounts) {
         messageProcessService.processOnMessage(expireAccounts);
     }
 
-    @RabbitListener(queues = {"#{disabledAccountsQueue.getName()}"})
+    @RabbitListener(queues = {"${service.mq.queues.disabled-accounts}"})
     public void listenDisabledAccounts(DisabledAccounts disabledAccounts) {
         messageProcessService.processOnMessage(disabledAccounts);
     }
